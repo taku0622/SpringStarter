58fa9601d4a532d388a688403b682e4c4525089d
taku0622 <hayatsutkm@gmail.com>
2022-03-27 13:36:44 +0900

    done

--- src/main/java/com/example/demo/app/SampleController.java
+++ src/main/java/com/example/demo/app/SampleController.java
@@ -1,25 +1,24 @@
 package com.example.demo.app;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-/*
- * Add annotations here
- */
+@Controller
+@RequestMapping("/sample")
 public class SampleController {
-	
+
 // 	private final JdbcTemplate jdbcTemplate;
 
-// 	//Add an annotation here 
+// 	//Add an annotation here
 // 	public SampleController(JdbcTemplate jdbcTemplate) {
 // 		this.jdbcTemplate = jdbcTemplate;
 // 	}
-	
-	@GetMapping
-	public String test(Model model) {
-		
-		//hands-on
 
+	@GetMapping("/test")
+	public String test(Model model) {
+		model.addAttribute("title", "Inquiry Form");
 		return "test";
 	}
 
